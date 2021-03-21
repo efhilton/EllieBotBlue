40e1fc39f4fd24e34af4f42f1fd48dfa9f7bc47c
Edgar Hilton <edgar.hilton@gmail.com>
2021-03-21 17:59:52 -0500

    Assume that mosquitto is installed locally

--- Csharp/EllieBot.Main/NervousSystem/Consstants.cs
+++ Csharp/EllieBot.Main/NervousSystem/Consstants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public static string DEFAULT_TOPIC_FOR_COMMANDS = "elliebot/commands";
-        public static string DEFAULT_COMMUNICATIONS_ADDRESS = "192.168.1.135";
+        public static string DEFAULT_COMMUNICATIONS_ADDRESS = "localhost";
         public static int DEFAULT_COMMUNICATIONS_PORT = 1883;
     }
 }
