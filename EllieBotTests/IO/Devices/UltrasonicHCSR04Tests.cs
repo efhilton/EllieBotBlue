91be6d19e27c531942242158dfe484f45f3eb49c
Edgar Hilton <edgar.hilton@gmail.com>
2021-04-05 00:56:52 -0500

    Fix unit tests.

--- EllieBotTests/IO/Devices/UltrasonicHCSR04Tests.cs
+++ EllieBotTests/IO/Devices/UltrasonicHCSR04Tests.cs
@@ -5,9 +5,9 @@ namespace Tests {
     [TestFixture()]
     public class UltrasonicHCSR04Tests {
 
-        [TestCase(160729, 20)]
-        [TestCase(810875, 10)]
-        [TestCase(842022, 10)]
+        [TestCase(160729, 275.65023500000001d)]
+        [TestCase(810875, 1390.650625d)]
+        [TestCase(842022, 1444.06773d)]
         public void TestCalculateDistanceInCm(long tof, double cm) {
             double actual = UltrasonicHCSR04.CalculateDistanceInCm(tof);
             Assert.That(actual, Is.EqualTo(cm));
