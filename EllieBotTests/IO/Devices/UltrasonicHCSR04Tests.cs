b8a92d581bddd738cf268fb5e6afc57406b5ba08
Edgar Hilton <edgar.hilton@gmail.com>
2021-04-10 14:42:53 -0500

    Remove dependency on device frequency.

--- EllieBotTests/IO/Devices/UltrasonicHCSR04Tests.cs
+++ EllieBotTests/IO/Devices/UltrasonicHCSR04Tests.cs
@@ -5,11 +5,11 @@ namespace Tests {
     [TestFixture()]
     public class UltrasonicHCSR04Tests {
 
-        [TestCase(160729, 0.803645)]
-        [TestCase(810875, 4.054375)]
-        [TestCase(842022, 4.21011)]
+        [TestCase(160729, 16072900)]
+        [TestCase(810875, 81087500)]
+        [TestCase(842022, 84202200)]
         public void TestCalculateDistanceInCm(long tof, double cm) {
-            double actual = UltrasonicHCSR04.CalculateDistanceInCm(tof, 1);
+            double actual = UltrasonicHCSR04.CalculateDistanceInCm(tof, 2.0, 1);
             Assert.That(actual, Is.EqualTo(cm));
         }
     }
